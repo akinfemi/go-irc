50682d483aee85dffed0435a029cc7378f6075b3
akinfemi <akinfemi.aluko@gmail.com>
2019-02-06 11:53:17 -0800

    client /connect

--- client/gui.go
+++ client/gui.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
+	"strings"
+	"sync"
 
+	"github.com/akinfemi/go-irc/server"
 	"github.com/jroimartin/gocui"
 )
 
+//Client : Mother Struct
+type Client struct {
+	connected bool
+	conn      net.Conn
+	sv        *server.Server
+	mutex     sync.Mutex
+}
+
+var client = &Client{connected: false, conn: nil, sv: nil}
+
+//Input Text box
 type Input struct {
 	name      string
 	x, y      int
@@ -13,6 +29,7 @@ type Input struct {
 	maxLength int
 }
 
+//BoxView each view
 type BoxView struct {
 	name      string
 	x, y      int
@@ -20,14 +37,17 @@ type BoxView struct {
 	maxLength int
 }
 
+//NewInput new textbox
 func NewInput(name string, x, y, w, maxLength int) *Input {
 	return &Input{name: name, x: x, y: y, w: w, maxLength: maxLength}
 }
 
+//NewBoxView : new views
 func NewBoxView(name string, x, y, w, h int) *BoxView {
 	return &BoxView{name: name, x: x, y: y, w: w, h: h}
 }
 
+//Layout : Each view must implement a Layout function
 func (b *BoxView) Layout(g *gocui.Gui) error {
 	v, err := g.SetView(b.name, b.x, b.y, b.w, b.h)
 	if err != nil {
@@ -39,6 +59,7 @@ func (b *BoxView) Layout(g *gocui.Gui) error {
 	return nil
 }
 
+//Layout : Layout function for textbox
 func (i *Input) Layout(g *gocui.Gui) error {
 	v, err := g.SetView(i.name, i.x, i.y, i.w, i.y+2)
 	if err != nil {
@@ -51,6 +72,7 @@ func (i *Input) Layout(g *gocui.Gui) error {
 	return nil
 }
 
+//Edit : Editable views must implement Edit
 func (i *Input) Edit(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
 	switch {
 	case ch != 0 && mod == 0:
@@ -62,6 +84,7 @@ func (i *Input) Edit(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) 
 	}
 }
 
+//SetFocus : Focus on view
 func SetFocus(name string) func(g *gocui.Gui) error {
 	return func(g *gocui.Gui) error {
 		_, err := g.SetCurrentView(name)
@@ -77,7 +100,7 @@ func main() {
 	defer g.Close()
 
 	g.Cursor = true
-	maxX, maxY := g.Size()
+	maxX, maxY := g.Size() //get screen size
 	chat := NewBoxView("chat", maxX/4+1, 0, maxX-1, maxY-5)
 	channels := NewBoxView("channels", 0, 0, maxX/4, maxY/2-1)
 	users := NewBoxView("users", 0, maxY/2+1, maxX/4, maxY-2)
@@ -89,20 +112,44 @@ func main() {
 	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
 		log.Panicln(err)
 	}
-	// if err := g.SetKeybinding("input", gocui.KeyEnter, gocui.ModNone, read); err != nil {
-	// 	log.Panicln(err)
-	// }
+	if err := g.SetKeybinding("input", gocui.KeyEnter, gocui.ModNone, read); err != nil {
+		log.Panicln(err)
+	}
 
 	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
 		log.Panicln(err)
 	}
 }
 
-// func read(g *gocui.Gui, v *gocui.View) error {
-// 	userInput := v.Buffer()
-// 	v.Clear()
+func runCommand(g *gocui.Gui, userInput string) {
+	tokens := strings.Split(userInput, " ")
+	if tokens[0] == "/connect" {
+		if !client.connected {
+			conn, err := net.Dial("tcp4", string(tokens[1]))
+			if err != nil {
+				log.Panicln(err)
+			}
+			client.mutex.Lock()
+			client.connected = true
+			client.conn = conn
+			client.mutex.Unlock()
+		}
+	} else {
+		fmt.Fprintf(client.conn, userInput)
+	}
+}
 
-// }
+func read(g *gocui.Gui, v *gocui.View) error {
+	userInput := strings.TrimSpace(v.Buffer())
+	v.Clear()
+	inputView, err := g.View("input")
+	if err != nil {
+		log.Panicln(err)
+	}
+	v.SetCursor(inputView.Origin())
+	go runCommand(g, userInput)
+	return nil
+}
 
 func quit(g *gocui.Gui, v *gocui.View) error {
 	return gocui.ErrQuit
